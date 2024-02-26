3c341984329cc713b5674682c880b5e40b6f4e01
Rumpa Giri <codekunoichi@users.noreply.github.com>
2024-02-26 17:50:56 +0000

    edit comments

--- csv_manipulation/find_same_payorid.py
+++ csv_manipulation/find_same_payorid.py
@@ -10,7 +10,7 @@ common_payors = pd.merge(md_online_df, change_healthcare_df, on='PayorID', suffi
 # To display the common payors, you can use:
 print(common_payors[['Payer Name_MDOnline', 'PayorID', 'Payer Name_ChangeHealthcare']].head())
 
-# If you want to save this to a CSV file for easier review:
+# save this to a CSV file for easier review:
 common_payors[['Payer Name_MDOnline', 'PayorID', 'Payer Name_ChangeHealthcare']].to_csv('common_payors.csv', index=False)
 
 # Eliminate duplicate triplets
